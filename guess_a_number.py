6cd00eeab6475e8341d9a86fd74524afee124256
Woods <rwoods1837@greenville.k12.sc.us>
2017-09-15 09:14:10 -0400

    made it deny the numbers

--- guess_a_number.py
+++ guess_a_number.py
@@ -12,10 +12,20 @@ print("I'm thinking of a number from " + str(low) + " to " + str(high) + ".");
 guess = -1
 tries = 0
 
+#helper functions
+def get_guess():
+    while True:
+        g = input("Take A Guess: ")
+
+        if g.isnumeric():
+            g = int(g)
+            return g
+        else:
+            print("YOU MUST ENTER A NUMBER")
+
 #play the game
 while guess != rand and tries < limit:
-    guess = input("Take a guess: ")
-    guess = int(guess)
+    guess = get_guess()
     
     if guess < rand:
         print("You guessed too low.")
@@ -28,4 +38,4 @@ while guess != rand and tries < limit:
 if guess == rand:
     print("Good job boi, you got it!")
 else:
-    print("aaaaaaaaaaaaand you failed, the number was " + str(rand))
+    print("YEET, the number was " + str(rand))
